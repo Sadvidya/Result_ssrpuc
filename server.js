3fcf8e8de626b85aa40a013eab5822ca84f00809
Sadvidya educational institutions <161982803+Sadvidya@users.noreply.github.com>
2024-08-14 18:55:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // MySQL connection
 const connection = mysql.createConnection({
